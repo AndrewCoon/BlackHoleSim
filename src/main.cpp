f78c634b8755fafaee917ded8218d7a7a1f4f1f6
Andrew Coon <andrew.coon84@gmail.com>
2025-10-14 15:49:00 -0600

    First Window

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,54 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
 #include <iostream>
 using namespace glm;
 using namespace std;
 
-int main() {
-  while (!glfwWindowShouldClose(window)) {
-    glClearColor(0.1f, 0.2f, 0.3f, 1.0f);
+struct Engine {
+public:
+  GLFWwindow* window;
+
+  int WIDTH = 800;
+  int HEIGHT = 600;
+
+  Engine() {
+    this->window=StartGLFW();
+  }
+  GLFWwindow* StartGLFW() {
+    if(!glfwInit()) {
+      cerr << "glfw failed to initialize\n";
+      return nullptr;
+    }
+
+    GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "Ray Tracer", NULL, NULL);
+    glfwMakeContextCurrent(window);
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+  }
+
+  void run() {
     glClear(GL_COLOR_BUFFER_BIT);
-    glfwSwapBuffers(window);
+    glMatrixMode(GL_PROJECTION);
+    double left = -WIDTH;
+    double right = WIDTH;
+    double bottom = -HEIGHT;
+    double top = HEIGHT;
+
+    glOrtho(left, right, bottom, top, -1.0, 1.0);
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+  }
+};
+Engine engine;
+
+int main() {
+  while (!glfwWindowShouldClose(engine.window)) {
+    engine.run();
+
+    glfwSwapBuffers(engine.window);
     glfwPollEvents();
   }
 
@@ -18,28 +58,4 @@ int main() {
 }
 
 
-Engine() {
-  if(!glfwInit()) {
-    cerr << "glfw failed to initialize\n";
-    return -1;
-  }
 
-  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
-  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-
-  GLFWwindow* window = glfwCreateWindow(800, 600, "Gravity Sim", NULL, NULL);
-  if(!window) {
-    cerr << "Window failed to create\n";
-    glfwTerminate();
-    return -1;
-  }
-
-  glfwMakeContextCurrent(window);
-
-
-  if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
-      std::cerr << "Failed to initialize GLAD\n";
-      return -1;
-  }
-}
