e3f4610e5ed12d89b2508fc1d46564269d1cbd6c
Andrew Coon <andrew.coon84@gmail.com>
2025-10-16 22:39:51 -0600

    implement object class

--- src/main.cpp
+++ src/main.cpp
@@ -7,16 +7,80 @@ using namespace std;
 
 #define PI 3.14159265359
 
+const int RES = 100;
 const int WIDTH = 800;
 const int HEIGHT = 600;
+const int TOP = HEIGHT / 2;
+const int LEFT = -WIDTH / 2;
+
+class Object {
+public:
+  vec2 position;
+  vec2 velocity;
+  vec2 acceleration;
+  float radius;
+  vec3 color;
+
+  Object(vec2 _pos, vec2 _vel, vec2 _accel, float _r, vec3 _col) {
+    this->position = _pos;
+    this->velocity = _vel;
+    this->acceleration = _accel;
+    this->radius = _r;
+    this->color = _col;
+  }
+
+  void accelerate() {
+    this->velocity += acceleration;
+  }
+
+  void updatePosition() {
+    this->position += velocity;
+  }
+
+  void step() {
+    this->accelerate();
+    this->updatePosition();
+  }
+
+  void checkCollisions() {
+    if (this->position.y < -TOP || this->position.y > TOP) {
+      this->velocity.y *= -0.95;
+    }
+
+    if (this->position.x < LEFT || this->position.x > -LEFT) {
+      this->velocity.x *= -0.95;
+    }
+  }
+
+  void draw(int _resolution) {
+    glColor3f(this->color.x, this->color.y, this->color.z);
+    glBegin(GL_TRIANGLE_FAN);
+    glVertex2d(this->position.x, this->position.y);
+
+    for (int i = 0; i <= _resolution; i++) {
+      float angle = 2.0f * PI * (static_cast<float>(i) / _resolution);
+      float x = this->position.x + cos(angle) * this->radius;
+      float y = this->position.y + sin(angle) * this->radius;
+      glVertex2d(x, y);
+    }
+    glEnd();
+  }
+};
 
 class Engine {
 public:
   GLFWwindow* window;
+  vector<Object> objects;
 
   Engine() {
     this->window=StartGLFW();
   }
+
+  vector<Object> setObjects(vector<Object> _n) {
+    this->objects = _n;
+    return this->objects;
+  }
+
   GLFWwindow* StartGLFW() {
     if(!glfwInit()) {
       cerr << "glfw failed to initialize\n";
@@ -47,6 +111,14 @@ public:
     glViewport(0, 0, WIDTH, HEIGHT);
     glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 
+
+    glMatrixMode(GL_PROJECTION);
+    double left = -WIDTH / 2.0;
+    double right = WIDTH / 2.0;
+    double bottom = -HEIGHT / 2.0;
+    double top = HEIGHT / 2.0;
+    glOrtho(left, right, bottom, top, -1.0, 1.0);
+
     return window;
   }
   void run() {
@@ -54,36 +126,14 @@ public:
     render();
   }
   
-  void drawCircle(vec2 _center, int _radius, int _resolution) {
-    glBegin(GL_TRIANGLE_FAN);
-    glVertex2d(_center.x, _center.y);
-
-    for (int i = 0; i <= _resolution; i++) {
-      float angle = 2.0f * PI * (static_cast<float>(i) / _resolution);
-      float x = _center.x + cos(angle) * _radius;
-      float y = _center.y + sin(angle) * _radius;
-      glVertex2d(x, y);
-    }
-    glEnd();
-  }
 
   void render() {
-
     glClear(GL_COLOR_BUFFER_BIT);
-    glMatrixMode(GL_PROJECTION);
-    double left = -WIDTH / 2.0;
-    double right = WIDTH / 2.0;
-    double bottom = -HEIGHT / 2.0;
-    double top = HEIGHT / 2.0;
 
-    glOrtho(left, right, bottom, top, -1.0, 1.0);
-    glMatrixMode(GL_MODELVIEW);
-    glLoadIdentity();
-
-    vec2 test(0.0f,0.0f);
-    glColor3f(1.0f, 0.0f, 0.0f);
-
-    drawCircle(test, 1, 100);
+    for(auto& obj:this->objects) {
+//      obj.step();
+      obj.draw(RES);
+    }
   }
 private:
   void processInput(GLFWwindow *window) {
@@ -94,9 +144,17 @@ private:
      glViewport(0, 0, width, height);
   }
 };
-Engine engine;
 
 int main() {
+  Engine engine;
+
+  vector<Object> objs = {
+    Object(vec2(-100, -100), vec2(0, 0), vec2(0, -9.8), 25.0f, vec3(190.0f / 255.0f, 0.0f, 0.0f)),
+    Object(vec2(100, 100), vec2(0, 0), vec2(0, -9.8), 25.0f, vec3(0.0f, 106.0f, 200.0f))
+  };
+
+  engine.setObjects(objs);
+
   while (!glfwWindowShouldClose(engine.window)) {
     engine.run();
 
