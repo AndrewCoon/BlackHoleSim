4bf14d98b9b22d3a70eef356578864d9d9541e0f
Andrew Coon <andrew.coon84@gmail.com>
2025-10-16 21:30:10 -0600

    add drawCircle function

--- build/cmake_install.cmake
+++ build/cmake_install.cmake
@@ -1,8 +1,8 @@
-# Install script for directory: C:/Users/Andre/Code/BlackHoleSim
+# Install script for directory: /home/andrew/Work/cpp/learn-opengl
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
-  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/OpenGL_Project")
+  set(CMAKE_INSTALL_PREFIX "/usr/local")
 endif()
 string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
 
@@ -12,7 +12,7 @@ if(NOT DEFINED CMAKE_INSTALL_CONFIG_NAME)
     string(REGEX REPLACE "^[^A-Za-z0-9_]+" ""
            CMAKE_INSTALL_CONFIG_NAME "${BUILD_TYPE}")
   else()
-    set(CMAKE_INSTALL_CONFIG_NAME "Release")
+    set(CMAKE_INSTALL_CONFIG_NAME "")
   endif()
   message(STATUS "Install configuration: \"${CMAKE_INSTALL_CONFIG_NAME}\"")
 endif()
@@ -27,25 +27,35 @@ if(NOT CMAKE_INSTALL_COMPONENT)
   endif()
 endif()
 
+# Install shared libraries without execute permission?
+if(NOT DEFINED CMAKE_INSTALL_SO_NO_EXE)
+  set(CMAKE_INSTALL_SO_NO_EXE "0")
+endif()
+
 # Is this installation the result of a crosscompile?
 if(NOT DEFINED CMAKE_CROSSCOMPILING)
   set(CMAKE_CROSSCOMPILING "FALSE")
 endif()
 
-if(NOT CMAKE_INSTALL_LOCAL_ONLY)
-  # Include the install script for the subdirectory.
-  include("C:/Users/Andre/Code/BlackHoleSim/build/_deps/glfw-build/cmake_install.cmake")
+# Set path to fallback-tool for dependency-resolution.
+if(NOT DEFINED CMAKE_OBJDUMP)
+  set(CMAKE_OBJDUMP "/usr/bin/objdump")
 endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for the subdirectory.
-  include("C:/Users/Andre/Code/BlackHoleSim/build/_deps/glm-build/cmake_install.cmake")
+  include("/home/andrew/Work/cpp/learn-opengl/build/_deps/glfw-build/cmake_install.cmake")
+endif()
+
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for the subdirectory.
+  include("/home/andrew/Work/cpp/learn-opengl/build/_deps/glm-build/cmake_install.cmake")
 endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
 if(CMAKE_INSTALL_LOCAL_ONLY)
-  file(WRITE "C:/Users/Andre/Code/BlackHoleSim/build/install_local_manifest.txt"
+  file(WRITE "/home/andrew/Work/cpp/learn-opengl/build/install_local_manifest.txt"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
 endif()
 if(CMAKE_INSTALL_COMPONENT)
@@ -61,6 +71,6 @@ else()
 endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
-  file(WRITE "C:/Users/Andre/Code/BlackHoleSim/build/${CMAKE_INSTALL_MANIFEST}"
+  file(WRITE "/home/andrew/Work/cpp/learn-opengl/build/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
 endif()
--- src/main.cpp
+++ src/main.cpp
@@ -5,12 +5,14 @@
 using namespace glm;
 using namespace std;
 
-struct Engine {
+#define PI 3.14159265359
+
+const int WIDTH = 800;
+const int HEIGHT = 600;
+
+class Engine {
 public:
   GLFWwindow* window;
-
-  int WIDTH = 800;
-  int HEIGHT = 600;
 
   Engine() {
     this->window=StartGLFW();
@@ -21,25 +23,75 @@ public:
       return nullptr;
     }
 
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 2);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_ANY_PROFILE);
+ 
     GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "Ray Tracer", NULL, NULL);
-    glfwMakeContextCurrent(window);
+    if (!window) {
+        cerr << "Failed to create GLFW window\n";
+        glfwDestroyWindow(window);
+        glfwTerminate();
+        exit(EXIT_FAILURE);
+    }
 
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwMakeContextCurrent(window);
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+      cerr << "Failed to initialize GLAD\n";
+      glfwDestroyWindow(window);
+      glfwTerminate();
+      return nullptr;
+    }
+    glViewport(0, 0, WIDTH, HEIGHT);
+    glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+
+    return window;
+  }
+  void run() {
+    processInput(this->window);
+    render();
+  }
+  
+  void drawCircle(vec2 _center, int _radius, int _resolution) {
+    glBegin(GL_TRIANGLE_FAN);
+    glVertex2d(_center.x, _center.y);
+
+    for (int i = 0; i <= _resolution; i++) {
+      float angle = 2.0f * PI * (static_cast<float>(i) / _resolution);
+      float x = _center.x + cos(angle) * _radius;
+      float y = _center.y + sin(angle) * _radius;
+      glVertex2d(x, y);
+    }
+    glEnd();
   }
 
-  void run() {
+  void render() {
+
     glClear(GL_COLOR_BUFFER_BIT);
     glMatrixMode(GL_PROJECTION);
-    double left = -WIDTH;
-    double right = WIDTH;
-    double bottom = -HEIGHT;
-    double top = HEIGHT;
+    double left = -WIDTH / 2.0;
+    double right = WIDTH / 2.0;
+    double bottom = -HEIGHT / 2.0;
+    double top = HEIGHT / 2.0;
 
     glOrtho(left, right, bottom, top, -1.0, 1.0);
     glMatrixMode(GL_MODELVIEW);
     glLoadIdentity();
+
+    vec2 test(0.0f,0.0f);
+    glColor3f(1.0f, 0.0f, 0.0f);
+
+    drawCircle(test, 1, 100);
+  }
+private:
+  void processInput(GLFWwindow *window) {
+    if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+      glfwSetWindowShouldClose(window, true);
+  }
+  static void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
+     glViewport(0, 0, width, height);
   }
 };
 Engine engine;
@@ -53,7 +105,6 @@ int main() {
   }
 
   glfwTerminate();
-
   return 0;
 }
 
